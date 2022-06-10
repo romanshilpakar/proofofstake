f25927c27f481c682594004603c5da15910d8fe1
romanshilpakar <iamromanshilpakar@gmail.com>
2022-06-10 08:18:20 +0545

    rest api

--- Main.py
+++ Main.py
@@ -12,11 +12,8 @@ import sys
 if __name__ == '__main__':
     ip = sys.argv[1]
     port = int(sys.argv[2])
+    apiPort = int(sys.argv[3])
 
     node = Node(ip, port)
     node.startP2P()
-
-    # if port == 10002:
-    #     node.p2p.connect_with_node('localhost', 10001)
-
-    
+    node.startAPI(apiPort)
--- Node.py
+++ Node.py
@@ -2,6 +2,9 @@ from Blockchain import Blockchain
 from TransactionPool import TransactionPool
 from Wallet import Wallet
 from SocketCommunication import SocketCommunication
+from NodeAPI import NodeAPI
+from Message import Message
+from BlockchainUtils import BlockchainUtils
 
 
 class Node():
@@ -16,4 +19,23 @@ class Node():
 
     def startP2P(self):
         self.p2p = SocketCommunication(self.ip, self.port)
-        self.p2p.startSocketCommunication()
+        self.p2p.startSocketCommunication(self)
+
+    def startAPI(self, apiPort):
+        self.api = NodeAPI()
+        self.api.injectNode(self)
+        self.api.start(apiPort)
+
+    def handleTransaction(self, transaction):
+        data = transaction.payload()
+        signature = transaction.signature
+        signerPublicKey = transaction.senderPublicKey
+        signatureValid = Wallet.signatureValid(
+            data, signature, signerPublicKey)
+        transactionExists = self.transactionPool.transactionExists(transaction)
+        if not transactionExists and signatureValid:
+            self.transactionPool.addTransaction(transaction)
+            message = Message(self.p2p.socketConnector,
+                              'TRANSACTION', transaction)
+            encodedMessage = BlockchainUtils.encode(message)
+            self.p2p.broadcast(encodedMessage)
